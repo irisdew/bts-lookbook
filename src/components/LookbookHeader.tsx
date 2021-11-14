b8e2d1eae8cae266c895e93069671b79351ec839
irisdew <ycleardew@gmail.com>
2021-11-14 17:50:07 +0900

    Remove prev header codes

--- src/components/LookbookHeader.tsx
+++ src/components/LookbookHeader.tsx
@@ -5,10 +5,6 @@ interface LookBookHeaderProps {
   memberName: string;
 }
 
-interface IMemberName {
-  len: number;
-}
-
 const LookBookHeader: React.FC<LookBookHeaderProps> = ({ memberName }) => {
   return (
     <HeaderContainer>
