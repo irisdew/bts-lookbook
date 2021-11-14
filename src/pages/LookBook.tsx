e34fd849b18744ec4cc0da611925241b81d9a943
irisdew <ycleardew@gmail.com>
2021-11-14 16:35:28 +0900

    Remove swiper

--- src/pages/LookBook.tsx
+++ src/pages/LookBook.tsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
 import LookbookInfo from "../components/LookbookInfo";
 import LookbookMain from "../components/LookbookMain";
 import LookbookPicker from "../components/LookbookPicker";
@@ -13,18 +10,6 @@ const LookBook: React.FC = () => {
       <LookbookInfo memberName={memberName} />
       <LookbookMain />
       <LookbookPicker setMemberName={setMemberName} />
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        centeredSlides
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper>
     </>
   );
 };
